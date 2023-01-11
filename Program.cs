a7a26edd9945f45b8fc2f7b7b97cc77cf6cbd8a4
rush7777 <rushhardwell@gmail.com>
2023-01-11 20:20:35 +0500

    Switch and Switch Expressions

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿// Calculator
-
-using System;
 System.Console.WriteLine($"Enter 1st number: " );
 int firsta = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter Operations(+,-,*,/,%)");
+string operators = Console.ReadLine();
 System.Console.WriteLine($"Enter 2nd number: " );
 int seconda = Convert.ToInt32(Console.ReadLine());
 
@@ -10,6 +10,13 @@ Console.WriteLine($"Addtraction: {firsta + seconda}");
 Console.WriteLine($"Subtraction: {firsta - seconda}");
 Console.WriteLine($"Multiplication: {firsta * seconda}");
 Console.WriteLine($"Division: {firsta / seconda}");
+//Ternar operators
+string massage =
+  firsta <= 0
+    ? "1st  number is negative"
+    : "1st number is positive";
+System.Console.WriteLine(massage);
+
 if(firsta == seconda){
 System.Console.WriteLine($"1st number is equal to 2nd number");
 }
@@ -20,3 +27,32 @@ else if(firsta < seconda){System.Console.WriteLine($"1st number is smaller than 
 else {
   System.Console.WriteLine($"1st number is not equal to 2nd number");
 }
+
+string results = operators switch
+{
+  "+" => $"{firsta} + {seconda} = {firsta + seconda}",
+  "-" => $"{firsta} - {seconda} = {firsta - seconda}",
+  "*" => $"{firsta} * {seconda} = {firsta * seconda}",
+  "/" => $"{firsta} / {seconda} = {firsta / seconda}",
+   _ => "Operation Not Found!"
+};
+System.Console.WriteLine(results);
+/*
+switch (operators)
+{
+    case "+" :
+      System.Console.WriteLine($"{firsta} + {seconda}  = {firsta + seconda}");
+    break;
+    case "-" :
+      System.Console.WriteLine($"{firsta} - {seconda}  = {firsta - seconda}");
+    break;
+    case "*" :
+      System.Console.WriteLine($"{firsta} * {seconda}  = {firsta * seconda}");
+    break;
+    case "/" :
+      System.Console.WriteLine($"{firsta} / {seconda}  = {firsta / seconda}");
+    break;
+    default:
+       System.Console.WriteLine("Operation NOT Found!");
+    break;
+} */
